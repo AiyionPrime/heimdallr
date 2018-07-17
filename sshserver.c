e7b05c641c4333fadf431787576d17bc2e8612ea
aiyion.prime <git@aiyionpri.me>
2018-07-17 02:57:24 +0200

    implemented process_client()

--- sshserver.c
+++ sshserver.c
@@ -38,7 +38,7 @@ int run_ssh_server(int port){
 				exit(-1);
 
 			case 0:
-				exit(authenticate());
+				exit(process_client());
 
 			default:
 				break;
@@ -47,6 +47,100 @@ int run_ssh_server(int port){
 	return EXIT_SUCCESS;
 }
 
-int authenticate() {
+int process_client() {
+	struct connection con;
+	con.session = session;
+
+	/* Perform key exchange. */
+	if (ssh_handle_key_exchange(con.session)) {
+		printf("Error exchanging keys: '%s'.\n", ssh_get_error(con.session));
+		return -1;
+	}
+
+	ssh_message message;
+	ssh_channel chan=0;
+	char buffer[2048];
+	int auth=0;
+	int exec=0;
+	int i;
+
+	/* proceed to authentication */
+	auth = authenticate(&con);
+	if(!auth){
+		printf("Authentication error: %s\n", ssh_get_error(session));
+		ssh_disconnect(session);
+		return 1;
+	}
+	do {
+		message = ssh_message_get(session);
+		if(message){
+			if(ssh_message_type(message) == SSH_REQUEST_CHANNEL_OPEN
+			&& ssh_message_subtype(message) == SSH_CHANNEL_SESSION) {
+				chan = ssh_message_channel_request_open_reply_accept(message);
+				ssh_message_free(message);
+				break;
+			} else {
+				ssh_message_reply_default(message);
+				ssh_message_free(message);
+			}
+		} else {
+			break;
+		}
+	} while(!chan);
+
+	if(!chan) {
+		printf("Error: Client did not ask for a channel session (%s)\n",
+		ssh_get_error(session));
+		ssh_finalize();
+		return 1;
+	}
+
+	do {
+		if(message != NULL) {
+			if(ssh_message_type(message) == SSH_REQUEST_CHANNEL) {
+				if (ssh_message_subtype(message) == SSH_CHANNEL_REQUEST_EXEC){
+					exec = 1;
+					ssh_message_channel_request_reply_success(message);
+					ssh_message_free(message);
+					continue;
+				}
+			}
+			ssh_message_reply_default(message);
+			ssh_message_free(message);
+		} else {
+			break;
+		}
+	} while(!exec);
+
+	if (exec) {
+		if ((i = ReadExec(chan, buffer, 1024)) > 0){
+			char buf[i];
+			snprintf(buf, sizeof buf, "%s", buffer);
+
+			show_content(&con, buf);
+			ssh_channel_write(chan,"INFO: Received keys, thanks for using heimdallr!\r\n",50);
+			ssh_channel_close(chan);
+			ssh_disconnect(session);
+			ssh_finalize();
+			return 0;
+		}
+	}
+
+	ssh_disconnect(session);
+
+	return 0;
+}
+
+int authenticate(struct connection *c) {
 	return EXIT_SUCCESS;
 }
+
+int ReadExec(ssh_channel chan, void *vptr, int maxlen) {
+	return 0;
+}
+
+int show_content(struct connection *c, char* command) {
+    printf("%s:\n%s\n", c->client_ip, command);
+    return 0;
+}
+
--- sshserver.h
+++ sshserver.h
@@ -1,9 +1,26 @@
 #ifndef SSHSERVER_H_
 #define SSHSERVER_H_
 
+#include <libssh/libssh.h>
+
+struct connection {
+	ssh_session session;
+	ssh_message message;
+	char client_ip[96];
+	char con_time[96];
+	char *user;
+	char *pass;
+};
+
 int run_ssh_server(int port);
 
-int authenticate();
+int process_client();
+
+int authenticate(struct connection *c);
+
+int ReadExec(ssh_channel chan, void *vptr, int maxlen);
+
+int show_content(struct connection *c, char* command);
 
 #endif //SSHSERVER_H_
 
