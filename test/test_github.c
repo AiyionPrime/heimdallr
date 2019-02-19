4ecee15f107158835b6b04506ad49b86c2bfc371
aiyion.prime <git@aiyionpri.me>
2019-02-20 00:56:43 +0100

    github: test added rg-broken_cache

--- test/test_github.c
+++ test/test_github.c
@@ -287,6 +287,34 @@ void test_read_githubkeys_unknown(void **state) {
 	free(fakepath_heap);
 }
 
+void test_read_githubkeys_broken_cache(void **state) {
+	(void) state;
+	int ret=-1;
+	struct UserPubkey *keys=NULL;
+
+	char* fakepath = "/test/home/testuser/.config/heimdallr/githubkeys/agithubuser";
+	char* fakepath_heap=malloc((strlen(fakepath)+1)*sizeof(char));
+	strcpy(fakepath_heap, fakepath);
+	struct dirent* directory;
+	directory = malloc(sizeof(struct dirent));
+	strcpy(directory->d_name, "somepubfile");
+
+	will_return(__wrap_get_githubuser_dir, fakepath_heap);
+	will_return(__wrap_opendir, 1);
+	will_return(__wrap_readdir, directory);
+	will_return(__wrap_ssh_pki_import_pubkey_file, -1);
+
+	will_return(__wrap_readdir, NULL);
+	will_return(__wrap_closedir, 0);
+
+	ret = read_githubkeys(&keys, "agithubuser");
+	free(directory);
+	free(fakepath_heap);
+
+	assert_null(keys);
+
+	assert_int_equal(0, ret);
+}
 
 void test_get_keys(void **state) {
 	(void) state;
@@ -381,6 +409,7 @@ int main (void)
 		cmocka_unit_test (test_read_githubkeys),
 		cmocka_unit_test (test_read_githubkeys_many),
 		cmocka_unit_test (test_read_githubkeys_unknown),
+		cmocka_unit_test (test_read_githubkeys_broken_cache),
 		cmocka_unit_test (test_reduce_slashes),
 		cmocka_unit_test (test_validate_githubname),
 	};
